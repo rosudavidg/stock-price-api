8d1355616605b828a9ac89dd5c04fb8e90d8d7a9
rosudavidg <rosudavidg@gmail.com>
2022-01-15 11:41:38 +0200

    Update documentation

--- stocks.py
+++ stocks.py
@@ -1,21 +1,30 @@
 import finnhub
-from secrets import FINNHUB_API_KEY
+from stocksclientsecrets import FINNHUB_API_KEY
 
 
 def get_finnhub_client(api_key):
+    '''
+    Creates a new client using API key
+    '''
 
     return finnhub.Client(api_key)
 
 
 def get_finnhub_price(client, symbol):
     '''
-    https://finnhub.io/docs/api/quote
+    Sends a request to Finnhub public API and retrieve the current price for a specific stock symbol
+
+    Finnhub documentation: https://finnhub.io/docs/api/quote
     '''
 
     return client.quote(symbol)['c']
 
 
 def get_price(symbol):
+    '''
+    Returns the current price for the given stock symbol
+    '''
+
     try:
         finnhub_client = get_finnhub_client(FINNHUB_API_KEY)
 
